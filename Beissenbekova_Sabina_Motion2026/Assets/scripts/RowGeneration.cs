80085f39c89f21d88ef61891f53d9fe181dae8d3
WIN-M1JFA527P1T\user <sabina060807@gmail.com>
2026-09-10 18:25:27 -0400

    made squares appear when the button is clicked. Every time the button is clicked, a square is added to the first square. When the number of squares achieve 5, it stops. There is also a text that showes the number of squares!

--- Beissenbekova_Sabina_Motion2026/Assets/scripts/RowGeneration.cs
+++ Beissenbekova_Sabina_Motion2026/Assets/scripts/RowGeneration.cs
@@ -1,7 +1,15 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RowGeneration : MonoBehaviour
 {
+    public int number;
+    public TextMeshProUGUI score;
+    float x;
+    float y = 0;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +20,56 @@ public class RowGeneration : MonoBehaviour
     void Update()
     {
         
+        Vector2 B = new Vector2(x-1, -1);
+        Vector2 L = new Vector2(x-1, y + 1);
+        Vector2 D = new Vector2(x + 1, y + 1);
+        Vector2 R = new Vector2(x + 1, y - 1);
+
+        Debug.DrawLine(B, L, Color.white, 50);
+        Debug.DrawLine(L, D, Color.white, 50);
+        Debug.DrawLine(D, R, Color.white, 50);
+        Debug.DrawLine(R, B, Color.white, 50);
+
+        if (number == 0)
+        {
+            x = 0;
+            
+        }
+        if(number == 1)
+        {
+            x = 2;
+            
+        }
+        if (number == 2)
+        {
+            x = 4;
+            
+        }
+        if (number == 3)
+        {
+            x = 6;
+            
+        }
+        if (number == 4)
+        {
+            x = 8;
+            
+        }
+        if (number == 5)
+        {
+            x = 10;
+            
+        }
+    }
+    
+
+    public void pickARandom()
+    {
+        number += 1;
+        if(number > 5)
+        {
+            number = 5;
+        }
+        score.text = number.ToString();
     }
 }
--- Beissenbekova_Sabina_Motion2026/Assets/scripts/SquareSpawner.cs
+++ Beissenbekova_Sabina_Motion2026/Assets/scripts/SquareSpawner.cs
@@ -8,8 +8,10 @@ public class SquareSpawner : MonoBehaviour
     public Vector2 R;
     public Vector2 B;
 
-    float x;
-    float y;
+    //public GameObject mouse;
+
+    //float x;
+    //float y;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,15 +23,20 @@ public class SquareSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        transform.position = mousePos;
-        mousePos.x = x;
-        mousePos.y = y;
+        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //transform.position = mousePos;
+        //mouse.x = x;
+        //mouse.y = y;
 
-        Vector2 B = new Vector2(x-1, y-1);
-        Vector2 L = new Vector2(x-1, y + 1);
-        Vector2 D = new Vector2(x + 1, y + 1);
-        Vector2 R = new Vector2(x + 1, y -1);
+        //Vector2 B = new Vector2(x - 1, -1);
+        //Vector2 L = new Vector2(x - 1, y + 1);
+        //Vector2 D = new Vector2(x + 1, y + 1);
+        //Vector2 R = new Vector2(x + 1, y - 1);
+
+        Vector2 B = new Vector2(- 1, -1);
+        Vector2 L = new Vector2(- 1, 1);
+        Vector2 D = new Vector2(1, 1);
+        Vector2 R = new Vector2(1, - 1);
 
         Debug.DrawLine(B, L, Color.white, 50);
         Debug.DrawLine(L, D, Color.white, 50);
