a543719a5557f14b8b78e80d3e5ea31e5e5349a6
WIN-M1JFA527P1T\user <sabina060807@gmail.com>
2026-09-10 21:19:02 -0400

    I made the line to be drawn using debug.drawline when and where the mouse is pressed and stop when and where the mouse is released. It doesn't really work correctly though

--- Beissenbekova_Sabina_Motion2026/Assets/scripts/Pipeline.cs
+++ Beissenbekova_Sabina_Motion2026/Assets/scripts/Pipeline.cs
@@ -1,7 +1,14 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pipeline : MonoBehaviour
 {
+    public bool isTheLeftMouseIsPressed = false;
+    public bool isTheLeftMouseIsReleased = false;
+    Vector2 startOfLine;
+    Vector2 endOfLine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,23 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isTheLeftMouseIsPressed = Mouse.current.leftButton.isPressed;
+        isTheLeftMouseIsReleased = Mouse.current.leftButton.wasReleasedThisFrame;
+
+
+        if (isTheLeftMouseIsPressed == true)
+        {
+            Debug.Log("The left mouse button is being held down");
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            startOfLine = mousePos;
+        }
+        if (isTheLeftMouseIsReleased == true)
+        {
+            Debug.Log("The left mouse button is released");
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            endOfLine = mousePos;
+        }
+
+        Debug.DrawLine(startOfLine, endOfLine, Color.yellow, 50);
     }
 }
